f325f2e68cc89ce5f3dd42abb985be05334c9e29
Daniel Amarante <daniel.amarante2@gmail.com>
2016-06-10 00:25:06 -0300

    Add commit msg file.

--- install.sh
+++ install.sh
@@ -3,4 +3,5 @@
 ln -sf $(pwd)/vim/.vimrc ~/.vimrc
 ln -sf $(pwd)/git/.gitconfig ~/.gitconfig
 ln -sf $(pwd)/git/.gitignore_global ~/.gitignore_global
+ln -sf $(pwd)/git/.git_commit_msg.txt ~/.git_commit_msg.txt
 ln -sf $(pwd)/zsh/.zshrc ~/.zshrc
