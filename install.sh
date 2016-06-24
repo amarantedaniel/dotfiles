8a709eb15d4da8ab4d10e6b7751766d912306371
Daniel Amarante <daniel.amarante2@gmail.com>
2016-06-24 00:08:10 -0300

    Add oh-my-zsh downloading to install script.

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+[ -d ~/.oh-my-zsh ] || git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh 
+
 ln -sf $(pwd)/vim/.vimrc ~/.vimrc
 ln -sf $(pwd)/git/.gitconfig ~/.gitconfig
 ln -sf $(pwd)/git/.gitignore_global ~/.gitignore_global
